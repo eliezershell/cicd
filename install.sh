3773390e1f170269721e5568b5415f17b2770310
Eliezer Ribeiro <146236086+eliezershell@users.noreply.github.com>
2024-12-05 22:47:17 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+### Instalação do Docker utilizando repositório https://github.com/eliezershell/docker.git
+git clone https://github.com/eliezershell/docker.git
+chmod +x ./docker/instalador_docker.sh; ./docker/instalador_docker.sh
+
+### Instalação do Jenkins
+#criando rede docker
+docker network create jenkins
+
+#instalando container 'docker in docker'
+docker run --name jenkins-docker-dind --rm --detach \
+  --privileged --network jenkins --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind --storage-driver overlay2
+
+  #COntruindo e instalando container Jenkins blue ocean
+  docker build -t myjenkins-blueocean:2.479.2-1 .
+
+  docker run --name jenkins-blueocean --restart=on-failure --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  myjenkins-blueocean:2.479.2-1
+  
