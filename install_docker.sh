4278f60228c286b6f03bc83a0c3da86f85a2901c
Eliezer Ribeiro <146236086+eliezershell@users.noreply.github.com>
2025-07-09 12:51:48 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Adicionando a chave GPG oficial do Docker
+# Adicionando chave GPG oficial Docker
 sudo apt update
 sudo apt install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo chmod 644 /etc/apt/keyrings/docker.gpg
 
-# Adicionando o repositório do Docker no APT
+# Adicionando o repositório Docker no APT
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
@@ -15,12 +15,8 @@ echo \
 sudo apt update
 
 # Instalando Docker e extensões necessárias
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+sudo apt install docker-ce docker-ce-cli docker-compose-plugin containerd.io docker-buildx-plugin -y
 
 # Ativando o serviço para iniciar junto com o S.O.
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
-
-# Adicionando usuário padrão ao grupo docker
-sudo usermod -aG docker $USER
-newgrp docker
